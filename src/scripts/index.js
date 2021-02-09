17f66f4a91ea81da616e99d2803a00cd919ed36f
yocosaka <sakayoco55@gmail.com>
2021-02-09 23:37:46 +0900

    Add menu and contact with Tabs - Add styles for tabs of menu and contact - Add temporary script for tabs - Add footer with copyright

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,9 +10,35 @@ import '../scss/styles.scss';
 
 // document.querySelector('body').appendChild(myIcon);
 
+// MENU
 const toggleMenu = document.querySelector('.hamburger');
 const headerNav = document.querySelector('.header__nav')
 toggleMenu.addEventListener('click', () => {
   toggleMenu.classList.toggle('open');
   headerNav.classList.toggle('active')
 })
+
+// TABS
+const items = document.querySelectorAll('menu__item')
+const tabs = document.querySelectorAll('.menu__tab')
+
+function openPage(id) {
+  document.querySelectorAll('.menu__tab').forEach(tab => {
+    tab.classList.remove('active')
+  })
+  document.querySelectorAll('.menu__item').forEach(item => {
+    item.classList.remove('active')
+  })
+
+  document.getElementById(id).classList.add('active')
+  document.querySelector(`div[data-id='${ id }']`).classList.add('active')
+}
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', e => {
+    openPage(e.target.getAttribute('data-id'))
+  })
+})
+
+
+document.getElementById("default-open").click();
